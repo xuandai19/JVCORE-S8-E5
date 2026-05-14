3bd463a1a6edae47dd0b66ec016a3537787a6be7
Xuân Đại <daik5xuanvinh@gmail.com>
2026-05-14 19:24:58 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,43 @@ public class Main {
 //        Book book = new Book("Java", "Nguyen Van A", 36.50);
 //        book.printInfo();
 
-        CurrencyConverter.setRate(25000);
+//        CurrencyConverter.setRate(25000);
+//
+//        int vnd = 500000;
+//
+//        double usd = CurrencyConverter.toUSD(vnd);
+//
+//        System.out.println("Tỉ giá: " + CurrencyConverter.getRate());
+//        System.out.println(vnd + " VND = " + CurrencyConverter.formatUSD(usd));
 
-        int vnd = 500000;
+        Rectangle r1 = new Rectangle(3, 4);
+        Rectangle r2 = new Rectangle(5, 2);
+        Rectangle r3 = new Rectangle(4.5, 3.5);
 
-        double usd = CurrencyConverter.toUSD(vnd);
+        System.out.println("Rectangle 1: " + r1);
+        System.out.println("Rectangle 2: " + r2);
+        System.out.println("Rectangle 3: " + r3);
 
-        System.out.println("Tỉ giá: " + CurrencyConverter.getRate());
-        System.out.println(vnd + " VND = " + CurrencyConverter.formatUSD(usd));
+        double maxArea = r1.getArea();
+        Rectangle maxRect = r1;
+        int rectNumber = 1;
+
+        if (r2.getArea() > maxArea) {
+            maxArea = r2.getArea();
+            maxRect = r2;
+            rectNumber = 2;
+        }
+
+        if (r3.getArea() > maxArea) {
+            maxArea = r3.getArea();
+            maxRect = r3;
+            rectNumber = 3;
+        }
+
+        System.out.println();
+        System.out.println("Largest area = " + maxArea
+                + " (Rectangle " + rectNumber
+                + ": width=" + maxRect.getWidth()
+                + ", height=" + maxRect.getHeight() + ")");
     }
 }
---
+++ src/Rectangle.java
@@ -0,0 +1,33 @@
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public String toString() {
+        return "width=" + width
+                + ", height=" + height
+                + ", area=" + getArea()
+                + ", perimeter=" + getPerimeter();
+    }
+}

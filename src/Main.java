343173e6a0e0113e95275b2756d8680e3d48c0ea
Xuân Đại <daik5xuanvinh@gmail.com>
2026-05-14 20:02:09 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Student student = new Student(1, "Nguyen Van A", 20);
@@ -15,34 +17,102 @@ public class Main {
 //        System.out.println("Tỉ giá: " + CurrencyConverter.getRate());
 //        System.out.println(vnd + " VND = " + CurrencyConverter.formatUSD(usd));
 
-        Rectangle r1 = new Rectangle(3, 4);
-        Rectangle r2 = new Rectangle(5, 2);
-        Rectangle r3 = new Rectangle(4.5, 3.5);
+//        Rectangle r1 = new Rectangle(3, 4);
+//        Rectangle r2 = new Rectangle(5, 2);
+//        Rectangle r3 = new Rectangle(4.5, 3.5);
+//
+//        System.out.println("Rectangle 1: " + r1);
+//        System.out.println("Rectangle 2: " + r2);
+//        System.out.println("Rectangle 3: " + r3);
+//
+//        double maxArea = r1.getArea();
+//        Rectangle maxRect = r1;
+//        int rectNumber = 1;
+//
+//        if (r2.getArea() > maxArea) {
+//            maxArea = r2.getArea();
+//            maxRect = r2;
+//            rectNumber = 2;
+//        }
+//
+//        if (r3.getArea() > maxArea) {
+//            maxArea = r3.getArea();
+//            maxRect = r3;
+//            rectNumber = 3;
+//        }
+//
+//        System.out.println();
+//        System.out.println("Largest area = " + maxArea
+//                + " (Rectangle " + rectNumber
+//                + ": width=" + maxRect.getWidth()
+//                + ", height=" + maxRect.getHeight() + ")");
 
-        System.out.println("Rectangle 1: " + r1);
-        System.out.println("Rectangle 2: " + r2);
-        System.out.println("Rectangle 3: " + r3);
+        Scanner sc = new Scanner(System.in);
 
-        double maxArea = r1.getArea();
-        Rectangle maxRect = r1;
-        int rectNumber = 1;
+        System.out.print("Nhập số sinh viên: ");
+        int n = sc.nextInt();
 
-        if (r2.getArea() > maxArea) {
-            maxArea = r2.getArea();
-            maxRect = r2;
-            rectNumber = 2;
-        }
+        Student[] students = new Student[n];
 
-        if (r3.getArea() > maxArea) {
-            maxArea = r3.getArea();
-            maxRect = r3;
-            rectNumber = 3;
-        }
+        do {
+            System.out.println("\n===== MENU SINH VIÊN =====");
+            System.out.println("1. Nhập danh sách sinh viên");
+            System.out.println("2. In danh sách sinh viên");
+            System.out.println("3. Tìm sinh viên GPA cao nhất");
+            System.out.println("4. In tổng số sinh viên đã tạo");
+            System.out.println("0. Thoát");
 
-        System.out.println();
-        System.out.println("Largest area = " + maxArea
-                + " (Rectangle " + rectNumber
-                + ": width=" + maxRect.getWidth()
-                + ", height=" + maxRect.getHeight() + ")");
+            System.out.print("lựa chọn của bạn: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    for (int i = 0; i < n; i++) {
+                        System.out.println("\nNhập sinh viên thứ " + (i + 1));
+
+                        students[i] = new Student();
+                        students[i].input();
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\nDanh sách sinh viên:");
+
+                    for (Student student : students) {
+                        if (student != null) {
+                            student.print();
+                        }
+                    }
+                    break;
+
+                case 3:
+                    Student maxStudent = students[0];
+
+                    for (int i = 1; i < n; i++) {
+                        if (students[i] != null &&
+                                students[i].getGpa() > maxStudent.getGpa()) {
+
+                            maxStudent = students[i];
+                        }
+                    }
+
+                    System.out.println("\nSinh viên GPA cao nhất:");
+                    maxStudent.print();
+                    break;
+
+                case 4:
+                    System.out.println("Tổng số sinh viên đã tạo: "
+                            + Student.getTotalStudent());
+                    break;
+
+                case 0:
+                    System.exit(0);
+
+                default:
+                    System.out.println("Lựa chọn không hợp lệ!");
+            }
+
+        } while (true);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,55 @@
+import java.util.Scanner;
+
 public class Student {
+//    private int id;
+//    private String name;
+//    private int age;
+//
+//    public Student(int id, String name, int age) {
+//        this.id = id;
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public void printInfo() {
+//        System.out.printf("ID: %d, Name: %s, Age: %d" , id, name, age);
+//    }
     private int id;
     private String name;
-    private int age;
+    private double gpa;
+    private static int countStudent;
 
-    public Student(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+    public Student() {
+        countStudent++;
     }
 
-    public void printInfo() {
-        System.out.printf("ID: %d, Name: %s, Age: %d" , id, name, age);
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    final double SCORE_FACTOR = 0.25;
+
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập mã sinh viên: ");
+        id = Integer.parseInt(sc.nextLine());
+        System.out.println("Nhập tên sinh viên: ");
+        name = sc.nextLine();
+        System.out.println("Nhập gpa sinh viên: ");
+        gpa = Double.parseDouble(sc.nextLine());
+    }
+
+    public void print() {
+        System.out.printf("Id: %d, Name: %s, GPA: %.2f", id, name, gpa);
+    }
+
+    public double getGpa(){
+        return gpa;
+    }
+
+    public static int getTotalStudent(){
+        return countStudent;
     }
 }
